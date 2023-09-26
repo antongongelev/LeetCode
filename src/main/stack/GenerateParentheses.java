f9aa39a616562c7cf05646007820e8978b1c9a20
antongongelev <antongongelev@gmail.com>
2023-09-26 12:25:20 +0300

    fix

--- src/main/stack/GenerateParentheses.java
+++ src/main/stack/GenerateParentheses.java
@@ -1,12 +1,11 @@
 package main.stack;
 
 import main.util.Medium;
-import main.util.MindBlowing;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class GenerateParentheses implements Medium, MindBlowing {
+public class GenerateParentheses implements Medium {
 
     public static void main(String[] args) {
         System.out.println(new GenerateParentheses().generateParenthesis(3));
